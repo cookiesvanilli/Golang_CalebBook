20ea07972f9021230128be318301302ec8989063
cookies <in.d2010@yandex.ru>
2021-08-02 21:43:16 +0300

    homework 9

--- hw9/hw9.go
+++ hw9/hw9.go
@@ -10,6 +10,7 @@ func main() {
 }
 type Shape interface {
 	area() float64
+	perimeter() float64
 }
 type MultiShape struct {
 	shapes []Shape
@@ -29,7 +30,9 @@ func distance(x1, y1, x2, y2 float64) float64 {
 func (c *Circle) area() float64 {
 	return math.Pi * c.r*c.r
 }
-
+func (c *Circle) perimeter() float64 {
+	return 2 * math.Pi * c.r
+}
 //Rectangle
 func (r *Rectangle) area() float64 {
 	l := distance(r.x1, r.y1, r.x1, r.y2)
@@ -37,8 +40,12 @@ func (r *Rectangle) area() float64 {
 	return l * w
 }
 
+func (r *Rectangle) perimeter() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return 2 * (l + w)
+}
 //Shape
-
 func (m *MultiShape) area() float64 {
 	var area float64
 	for _, s := range m.shapes {
@@ -53,9 +60,11 @@ func shapeMain() {
 	fmt.Println(c.area())
 	m := MultiShape{[]Shape{&r, &c}}
 	fmt.Println(m.area())
-}
 
-//-------------------->
+	fmt.Println(c.perimeter())
+	fmt.Println(r.perimeter())
+}
+//--------------------
 
 type Person struct {
 	Name string
@@ -92,7 +101,7 @@ func nameMain() {
 	p.Address.City = "Gotham"
 	p.Location()
 }
-//tasks------------------>
+//task1------------------>
 //Какая разница между методом и функцией?
 //Функция является независимой частью кода,
 //связывающей один или несколько входных параметров с одним или
@@ -108,3 +117,18 @@ A метод - это фрагмент кода, вызываемый имене
 Метод способен работать с данными, содержащимися в классе
 (помня, что объект является экземпляром класса - класс является определением, объект является экземпляром этих данных).
 */
+//task2------------------>
+//В каких случаях могут пригодиться встроенные (скрытые) поля?
+/*type Item struct {
+	id string // Именованное поле (агрегирование)
+	price float64 // Именованное поле (агрегирование)
+	quantity int // Именованное поле (агрегирование)
+}
+func (item *Item) Cost() float64 {
+	return item.price * float64(item.quantity)
+}
+type SpecialItem struct {
+	Item // Анонимное поле (встраивание)
+	catalogId int // Именованное поле (агрегирование)
+}*/
+
