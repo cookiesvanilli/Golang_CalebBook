370890583eb2bb49f897ab4b1c60e7dc9ddd8afa
cookies <in.d2010@yandex.ru>
2021-09-09 22:16:47 +0300

    test

--- hw11/math/math.go
+++ hw11/math/math.go
@@ -1,9 +1,41 @@
 package math
 
+import "log"
 func Average(xs []float64) float64 {
-	total := float64(0)
-	for _, x := range xs {
-		total += x
+	if len(xs) != 0 {
+		total := float64(0)
+		for _, x := range xs {
+			total += x
+		}
+		return total / float64(len(xs))
+	} else {
+		return 0
 	}
-	return total / float64(len(xs))
+}
+func Min(list []float64) float64 {
+	if len(list) == 0 {
+		return 0
+	}
+	min := list[0]
+	for _, x := range list {
+		if min > x {
+			min = x
+		}
+	}
+	return min
+}
+func Max(list []float64) float64 {
+	if len(list) == 0 {
+		return 0
+	}
+	max := list[0]
+	for _, x := range list {
+		if max < x {
+			max = x
+		}
+	}
+	if max != 15 {
+		log.Fatal("error")
+	}
+	return max
 }
